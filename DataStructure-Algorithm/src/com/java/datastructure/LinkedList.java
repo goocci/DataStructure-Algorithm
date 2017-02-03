ace15e3675e64619fdd5e7b52503f5b1aeadd56c
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-02-03 14:05:31 +0900

    LinkedList

--- DataStructure-Algorithm/src/com/java/datastructure/LinkedList.java
+++ DataStructure-Algorithm/src/com/java/datastructure/LinkedList.java
@@ -1,5 +1,13 @@
 package com.java.datastructure;
 
+/*
+ * 단순 연결 리스트(simple linked list, singly linked list)는 가장 단순한 연결 리스트의 형태로 각 노드들은 다음 노드를 가리키는 하나의 참조만을 갖는다. 
+다음 노드의 참조밖에 가지고 있지 않으므로 노드의 접근은 한 방향으로만 가능하다.
+
+헤더는 처음 노드의 참조만 가지고 있으며 처음 노드는 두번째 노드, 두번째 노드는 세번째 노드를 가리키고 있으며 마지막 노드가 가리키는 참조값은 null이 된다.
+즉, 헤더가 가리키는 노드가 처음 노드며 참조값이 null인 노드가 마지막 노드가 되는 것이다.
+*/
+
 public class LinkedList {
     
     private Node header;
