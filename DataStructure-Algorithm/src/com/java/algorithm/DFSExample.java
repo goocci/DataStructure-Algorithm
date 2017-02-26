1355c6b9c9ba6ff05b57cf382afb1fd3fae40c41
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-02-26 23:43:55 +0900

    [Modify] DFS : Shortest Distance Problem

--- DataStructure-Algorithm/src/com/java/algorithm/DFSExample.java
+++ DataStructure-Algorithm/src/com/java/algorithm/DFSExample.java
@@ -5,27 +5,30 @@ import java.util.Scanner;
 public class DFSExample {
 	
 	static int[][] map;
-	static int[][] visited;
-	static int map_size;
+	static int n;
+	static int m;
 	static int min;
 	
 	public static void main(String[] args){
 		
 		Scanner sc = new Scanner(System.in);
-		map_size = Integer.parseInt(sc.nextLine().trim());
-		map = new int[map_size+1][map_size+1];
-		min = map_size * map_size;
+		n = sc.nextInt();
+		m = sc.nextInt();
+		map = new int[n+1][m+1];
+		min = n * m;
 		
 		//이동할 수 있는 path를 입력받음
 		// 0 -> 길ㄴㄴ
 		// 1 -> 길ㅇㅇ
-		for(int i=0 ; i < map_size ; i++){
-			for(int j=0 ; j < map_size ; j++){
-				map[i][j] = sc.nextInt();
+		String nString = "";
+		for(int i = 0 ; i < n ; i++){
+			nString = sc.next();
+			for(int j = 0 ; j < m ; j++){
+				map[i][j] = Integer.parseInt(nString.charAt(j)+"");
 			}
 		}
 		
-		dfs(0,0,0);
+		dfs(0,0,1);
 		
 		System.out.println("최단거리: "+min);
 	}
@@ -33,40 +36,40 @@ public class DFSExample {
 	public static void dfs(int x, int y, int length){
 		
 		//도착
-		if(x == map_size-1 && y == map_size-1){
+		if(x == m-1 && y == n-1){
 			System.out.println("도착!!!"+length);
 			if(min > length) min = length;
 			return;
 		}
 		
-		map[x][y] = 0; //방문한 곳(현재위치)은 0 대입
+		map[y][x] = 0; //방문한 곳(현재위치)은 0 대입
 		
 		//왼쪽으로 이동할 경우
-		if(x > 0 && map[x-1][y] == 1){
+		if(x > 0 && map[y][x-1] == 1){
 			System.out.print("좌-");
 			dfs(x-1, y, length+1);
 		}
 		
 		//오른쪽으로 이동할 경우
-		if(x < map_size && map[x+1][y] == 1){
+		if(x < m && map[y][x+1] == 1){
 			System.out.print("우-");
 			dfs(x+1, y, length+1);
 		}
 		
 		//아래로 이동할 경우
-		if(y < map_size && map[x][y+1] == 1){
+		if(y < n && map[y+1][x] == 1){
 			System.out.print("아래-");
 			dfs(x, y+1, length+1);
 		}
 		
 		//위로 이동할 경우
-		if(y > 0 && map[x][y-1] == 1){
+		if(y > 0 && map[y-1][x] == 1){
 			System.out.print("위-");
 			dfs(x, y-1, length+1);
 		}
 		
 		System.out.println("막힘-");
-		map[x][y] = 1 ; //지나간 자리를 원상태로 돌리기 위해서 1 대입
+		map[y][x] = 1 ; //지나간 자리를 원상태로 돌리기 위해서 1 대입
 	}
 
 }
